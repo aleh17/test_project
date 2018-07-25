6c53c1d8832d2168885b2c56825e8dfc7beddc62
Aleh <test@test.com>
2018-07-25 21:51:56 +0300

    Security was added to api

--- test_project/src/main/java/com/training/test_project/security/config/WebSecurityConfig.java
+++ test_project/src/main/java/com/training/test_project/security/config/WebSecurityConfig.java
@@ -1,42 +1,58 @@
 package com.training.test_project.security.config;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .and()
-            .logout()
-            .permitAll()
-            .and()
-                .authorizeRequests()
-                .antMatchers("/", "/home").permitAll()
-                .anyRequest().authenticated();
+
+    @Autowired
+    DataSource dataSource;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    @Autowired
+    protected void configureAuthentication(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "SELECT employees.login as username, employees.password as password, employees.role as role FROM `employees` WHERE employees.login=?")
+                .authoritiesByUsernameQuery(
+                        "SELECT employees.login as username, employees.password as password, employees.role as role FROM `employees` WHERE employees.login=?")
+                .dataSource(dataSource)
+                .passwordEncoder(bCryptPasswordEncoder);
     }
 
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("user")
-                        .password("password")
-                        .roles("USER")
-                        .build();
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                //api access
+                .antMatchers("/api/departments/**").access("hasRole('TOP_MANAGER')")
+                .antMatchers("/api/employees/department**").access("hasAnyRole('TOP_MANAGER','DEPARTMENT_MANAGER')")
+                .antMatchers("/api/employees/info/{\\d+}").access("hasAnyRole('TOP_MANAGER','DEPARTMENT_MANAGER','EMPLOYEE')")
+                .antMatchers("/api/employees/info/update/**").access("hasRole('TOP_MANAGER')")
+                .antMatchers("/api/vacations/request").access("hasRole('EMPLOYEE')")
+                .antMatchers("/api/vacations/approve/**").access("hasAnyRole('TOP_MANAGER','DEPARTMENT_MANAGER')")
+                .antMatchers("/api/vacations/decline/**").access("hasAnyRole('TOP_MANAGER','DEPARTMENT_MANAGER')")
 
-        return new InMemoryUserDetailsManager(user);
+                //pages access
+                .antMatchers("/hello").access("hasRole('TOP_MANAGER')")
+                .anyRequest().permitAll()
+                .and()
+                .formLogin().loginPage("/login")
+                .usernameParameter("username").passwordParameter("password")
+                .and()
+                .logout().logoutSuccessUrl("/login?logout")
+                .and()
+                .exceptionHandling().accessDeniedPage("/403")
+                .and()
+                .csrf();
     }
 }
